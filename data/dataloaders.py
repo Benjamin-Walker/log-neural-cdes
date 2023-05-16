0c9a55952f1a94c3085f03030fd6331b42c0133c
ben walker <ben.walker@ladeck.com>
2023-05-16 14:16:47 +0100

    Added InMemoryDataloader class

--- data/dataloaders.py
+++ data/dataloaders.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 from typing import Any, Dict
 
+import jax.numpy as jnp
+import jax.random as jr
+
 
 @dataclass
 class Dataset:
@@ -9,3 +12,37 @@ class Dataset:
     sig_dataloaders: Dict[str, Any]
     data_dim: int
     label_dim: int
+
+
+class InMemoryDataloader:
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
+
+    def __iter__(self):
+        RuntimeError("Use .loop(batch_size) instead of __iter__")
+
+    def loop(self, batch_size, *, key):
+
+        if not isinstance(batch_size, int) & (batch_size > 0):
+            raise ValueError("Batch size must be a positive integer")
+
+        dataset_size = len(self.data)
+
+        if batch_size > dataset_size:
+            raise ValueError("Batch size larger than dataset size")
+        elif batch_size == dataset_size:
+            while True:
+                yield self.data, self.label
+        else:
+            indices = jnp.arange(dataset_size)
+            while True:
+                subkey, key = jr.split(key)
+                perm = jr.permutation(subkey, indices)
+                start = 0
+                end = batch_size
+                while end < dataset_size:
+                    batch_perm = perm[start:end]
+                    yield self.data[batch_perm], self.data[batch_perm]
+                    start = end
+                    end = start + batch_size
