42aece01bf1c53311ae11272b70b45e26bf797b9
ben walker <ben.walker@ladeck.com>
2023-05-19 11:46:17 +0100

    Added LogNeuralCDEs and modified signature calculation to use HallSet.

--- data/generate_paths.py
+++ data/generate_paths.py
@@ -1,6 +1,15 @@
 import jax
 import jax.numpy as jnp
-from signax.signature_flattened import logsignature
+from signax.signature import signature
+from signax.signature_flattened import flatten
+from signax.tensor_ops import log
+
+from data.hall_set import HallSet
+
+
+def hall_basis_logsig(x, depth, t2l):
+    logsig = flatten(log(signature(x, depth)))
+    return t2l[:, 1:] @ logsig
 
 
 def calc_paths(data, stepsize, depth=2):
@@ -12,16 +21,22 @@ def calc_paths(data, stepsize, depth=2):
     it is necessary to specify the stepsize and depth ahead of time.
     """
 
+    hs = HallSet(data.shape[-1], depth)
+    t2l = hs.t2l_matrix(depth)
+
     final_data = data[:, -(data.shape[1] % stepsize) - 1 :, ...]
     data = data[:, : -(data.shape[1] % stepsize), ...].reshape(
         data.shape[0], -1, stepsize, data.shape[-1]
     )
 
-    def calc_logsig(x):
-        return jnp.concatenate((jnp.array([0]), logsignature(x, depth)))
-
-    vmap_calc_logsig = jax.vmap(calc_logsig)
-    final_logsigs = vmap_calc_logsig(final_data)[:, None, :]
-    logsigs = jnp.concatenate((jax.vmap(vmap_calc_logsig)(data), final_logsigs), axis=1)
+    vmap_calc_logsig = jax.vmap(hall_basis_logsig, in_axes=(0, None, None))
+    final_logsigs = vmap_calc_logsig(final_data, depth, t2l)[:, None, :]
+    logsigs = jnp.concatenate(
+        (
+            jax.vmap(vmap_calc_logsig, in_axes=(0, None, None))(data, depth, t2l),
+            final_logsigs,
+        ),
+        axis=1,
+    )
 
     return logsigs
---
+++ models/LogNeuralCDEs.py
@@ -0,0 +1,97 @@
+import diffrax
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+import jax.random as jr
+
+from data.hall_set import HallSet
+from models.NeuralCDEs import VectorField
+
+
+class LogNeuralCDE(eqx.Module):
+    vf: VectorField
+    width: int
+    depth: int
+    hidden_dim: int
+    linear1: eqx.nn.Linear
+    linear2: eqx.nn.Linear
+    pairs: jnp.array
+    classification: bool
+    intervals: jnp.ndarray
+
+    def __init__(
+        self,
+        vf_hidden_dim,
+        vf_num_hidden,
+        hidden_dim,
+        data_dim,
+        depth,
+        label_dim,
+        classification,
+        intervals,
+        *,
+        key,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        vf_key, l1key, l2key = jr.split(key, 3)
+        vf = VectorField(
+            hidden_dim, hidden_dim * data_dim, vf_hidden_dim, vf_num_hidden, key=vf_key
+        )
+        self.vf = vf
+        self.width = data_dim
+        self.depth = depth
+        self.hidden_dim = hidden_dim
+        self.linear1 = eqx.nn.Linear(data_dim, hidden_dim, key=l1key)
+        self.linear2 = eqx.nn.Linear(hidden_dim, label_dim, key=l2key)
+        hs = HallSet(self.width, self.depth)
+        self.pairs = jnp.asarray(hs.data[1:])
+        self.classification = classification
+        self.intervals = intervals
+
+    def __call__(self, X):
+
+        ts, logsig, x0 = X
+        y0 = self.linear1(x0)
+
+        def func(t, y, args):
+            idx = jnp.searchsorted(self.intervals, t)
+            logsig_t = logsig[idx]
+            vf_out = jnp.reshape(self.vf(y), (self.width, self.hidden_dim))
+            jvps = jnp.reshape(
+                jax.vmap(lambda x: jax.jvp(self.vf, (y,), (x,))[1])(vf_out),
+                (self.width, self.width, self.hidden_dim),
+            )
+
+            def liebracket(jvps, pair):
+                return (
+                    jvps[pair[1] - 1, (pair[0] - 1)] - jvps[pair[0] - 1, (pair[1] - 1)]
+                )
+
+            lieout = jax.vmap(liebracket, in_axes=(None, 0))(
+                jvps, self.pairs[self.width :]
+            )
+
+            return jnp.dot(logsig_t[1 : self.width + 1], vf_out) + jnp.dot(
+                logsig_t[self.width + 1 :], lieout
+            )
+
+        if self.classification:
+            saveat = diffrax.SaveAt(t1=True)
+        else:
+            saveat = diffrax.SaveAt(ts=ts)
+
+        solution = diffrax.diffeqsolve(
+            diffrax.ODETerm(func),
+            diffrax.Tsit5(),
+            t0=ts[0],
+            t1=ts[-1],
+            dt0=None,
+            y0=y0,
+            stepsize_controller=diffrax.PIDController(rtol=1e-3, atol=1e-6),
+            saveat=saveat,
+        )
+        if self.classification:
+            return jax.nn.softmax(self.linear2(solution.ys[-1]))
+        else:
+            return jax.vmap(self.linear2)(solution.ys)
